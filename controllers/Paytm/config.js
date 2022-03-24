e4d23513b7a8711d4ebdd5598147752c3c33cf04
Vishal-hu <rajputvishal21598@gmail.com>
2022-03-24 22:49:16 +0530

    some changes

---
+++ controllers/Paytm/config.js
@@ -0,0 +1,7 @@
+var PaytmConfig = {
+    mid: "acNWHP42843987049185",
+    key: "3BvdZorU8#p0_KE&",
+    website: "WEBSTAGING",
+    callBackUrl:"https://lpg-demo-app.herokuapp.com/payment/callback"
+  };
+  module.exports.PaytmConfig = PaytmConfig;
