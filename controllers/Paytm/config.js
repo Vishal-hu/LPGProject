66c6ed979b8eed0315f8a49ffa1e978d63e2f717
Vishal-hu <72161375+Vishal-hu@users.noreply.github.com>
2022-12-31 17:13:29 +0530

    config removed

--- controllers/Paytm/config.js
+++ controllers/Paytm/config.js
@@ -1,9 +1,7 @@
 var PaytmConfig = {
-    mid: "EqKtrB24231889665332",
-    key: "O%XgDF6TQkHxm#qS",
+    mid: "",
+    key: "",
     website: "DEFAULT",
     callBackUrl:"https://lpg-demo-app.herokuapp.com/payment/callback"
   };
   module.exports.PaytmConfig = PaytmConfig;
-  
-  //acNWHP42843987049185, 3BvdZorU8#p0_KE&
